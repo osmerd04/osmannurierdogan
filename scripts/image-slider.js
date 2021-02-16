209209843b1f5a4e3a636e0665f417395414ab2e
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-02-16 18:48:15 +0300

    Js Path changed,

--- scripts/image-slider.js
+++ scripts/image-slider.js
@@ -6,6 +6,6 @@ let randomImage;
 const changeBackgroundImage = () => {
     console.log('OSMAN NURI ERDOGAN');
     randomImage = Math.floor(Math.random() * totalBackgrounds) + 1;
-    document.getElementsByTagName('body')[0].background = "../Images/" + randomImage + ".jpg";
+    document.getElementsByTagName('body')[0].background = "Images/" + randomImage + ".jpg";
 }
 setInterval(changeBackgroundImage, 2000);
