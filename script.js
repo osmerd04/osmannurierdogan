ddbee8685f2ca2eaa9d0804eea562dcdfb268b01
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-21 13:33:25 +0300

    Basics of Portfolio Have Been Completed.

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+
+const hello_about = document.querySelector('.hello_about');
+const me_work = document.querySelector('.me_work');
+const name_contact = document.querySelector('.name_contact');
+
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.me').addEventListener('mouseover', () => {
-        document.querySelector('.me').innerHTML = 'Work';
-    })
-    document.querySelector('.me').addEventListener('mouseleave', () => {
-        document.querySelector('.me').innerHTML = 'Ben';
+
+    hello_about.addEventListener('mouseover', () => {
+        hello_about.innerHTML = 'Hakkımda';
+    });
+
+    hello_about.addEventListener('mouseleave', () => {
+        hello_about.innerHTML = 'Merhaba.';
+    });
+
+    me_work.addEventListener('mouseover', () => {
+        me_work.innerHTML = 'Projeler';
+    });
+
+    me_work.addEventListener('mouseleave', () => {
+        me_work.innerHTML = 'Ben';
+    });
+
+    name_contact.addEventListener('mouseover', () => {
+        name_contact.innerHTML = 'İletişim';
+    });
+
+    name_contact.addEventListener('mouseleave', () => {
+        name_contact.innerHTML = 'Osman'
     })
 })
